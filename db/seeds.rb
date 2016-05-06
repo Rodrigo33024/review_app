245226eb3f07dcf938dff4a2feedb7bc4b973167
Rodrigo Recoba <Rodrigo33024@gmail.com>
2016-05-06 16:31:27 -0500

    Adds Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: 'Classical')
+Category.create(name: 'Drama')
+Category.create(name: 'Comedy')
